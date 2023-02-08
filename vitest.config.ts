90e84ec95fef4f4fc07c4fd18a75d10ee8761136
Payal17122000 <raviyapayal17@gmail.com>
2023-02-08 17:09:31 +0530

    fix: solve lintfix error

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
-//   test: {
-//     environment: 'happy-dom'
-//   },
+  //   test: {
+  //     environment: 'happy-dom',
+  //   },
 })
